0df2f9743cc1ff0dadd98f7cd1a693d39cc2125f
Dmitrii Samoilov <e2718281828@ya.ru>
2016-07-28 00:16:13 +0300

    added more parameters to products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,15 @@
 class ProductsController < ApplicationController
   respond_to :html, :json
+
 	skip_before_action :verify_authenticity_token
+
   inherit_resources
-  protected
+
+protected
+
 	def product_params
-		params.require(:product).permit(:id, :name, :price, :colors, 
-      :sizes, :permalink)
+		params.require(:product).permit(:id, :name, :description,
+			:sale_price, :has_popular, :has_sale, :in_stock, :quantity, 
+			:price, :colors, :sizes, :permalink)
 	end
 end
