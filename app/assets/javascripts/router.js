95c1751c5fe9de1e9c4cb69addfc1853bb9810cd
Vladimir Samojlov <v-moi@ya.ru>
2016-08-02 16:51:45 +0300

    without hashtag

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -19,7 +19,8 @@ angular
   .config([
     '$stateProvider',
     '$urlRouterProvider',
-    function($stateProvider, $urlRouterProvider) {
+    '$locationProvider',
+    function($stateProvider, $urlRouterProvider, $locationProvider) {
       $stateProvider
       .state('home', {
         url: '/', 
@@ -117,5 +118,15 @@ angular
         templateUrl: 'admin/addnewpage.html'
       });
       $urlRouterProvider.otherwise('/');
+      //check browser support
+      if(window.history && window.history.pushState){
+        //$locationProvider.html5Mode(true); will cause an error $location in HTML5 mode requires a  tag to be present! Unless you set baseUrl tag after head tag like so: <head> <base href="/">
+        // to know more about setting base URL visit: https://docs.angularjs.org/error/$location/nobase
+        // if you don't wish to set base URL then use this
+        $locationProvider.html5Mode({
+          enabled: true,
+          requireBase: false
+        });
+      }
     }
   ]);
