a2207f7a9c24e4fb6f0ad6daf6101148f36eeee7
Vladimir Samojlov <v-moi@ya.ru>
2016-08-09 23:19:06 +0300

    admin invoices

--- app/assets/javascripts/controllers/invoicesCtrl.js
+++ app/assets/javascripts/controllers/invoicesCtrl.js
@@ -1,7 +1,7 @@
 angular.module('MyStore').controller('invoicesCtrl', 
-  ['$scope', '$http', '$stateParams', '$location', 'Order',  invoicesCtrl]);
+  ['$scope', '$http', '$stateParams', '$state', '$location', 'Order',  invoicesCtrl]);
 
-function invoicesCtrl($scope, $http, $stateParams, $location, Order) {
+function invoicesCtrl($scope, $http, $stateParams, $state, $location, Order) {
   console.log('invoicesCtrl');
   
   $http.get('/orders/'+$stateParams.id+'.json').success(function(data, status, headers, config){
@@ -11,4 +11,9 @@ function invoicesCtrl($scope, $http, $stateParams, $location, Order) {
   Order.query({}, function(data){
     $scope.orders = data;
   });
+
+  $scope.orderDel = function(order_id){
+    Order.delete({id: order_id});
+    $state.go('admininvoices');
+  }
 }
