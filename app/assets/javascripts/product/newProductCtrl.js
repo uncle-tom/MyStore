aa9e8aad9270a8bf6ee92ed996a2a01fcd1e682f
Dmytro Samoilov <e2718281828@ya.ru>
2016-08-31 18:27:06 +0300

    fixed saving products

--- app/assets/javascripts/product/newProductCtrl.js
+++ app/assets/javascripts/product/newProductCtrl.js
@@ -20,4 +20,20 @@ function addNewProduct($scope, $http, $localStorage, $sessionStorage, Auth, Prod
       window.location.reload();
     })
   }
+
+  $scope.colors = [
+    {color: "Черный"},
+    {color: "Не черный"},
+    {color: "Белый"}
+  ]
+
+  $scope.sizes = [
+    {size: "70C"},
+    {size: "75C"},
+    {size: "80C"},
+    {size: "85C"},
+    {size: "70B"},
+    {size: "75B"},
+    {size: "80B"},
+  ];
 }
